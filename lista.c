dd5cfb609ce8a9b9da84e813cd32d15f487ef2c4
spenalva1 <penalvasantiagogm@gmail.com>
2020-11-06 18:29:49 -0300

    implementado y testeado hasta lista vacia

--- lista.c
+++ lista.c
@@ -23,7 +23,7 @@ int lista_insertar(lista_t* lista, void* elemento){
         return ERROR;
     nodo->elemento = elemento;
     nodo->siguiente = NULL;
-    if(lista->cantidad == 0){
+    if(lista_vacia(lista)){
         lista->nodo_inicio = nodo;
         lista->nodo_fin = nodo;
     }else{
@@ -38,7 +38,7 @@ int lista_insertar_en_posicion(lista_t* lista, void* elemento , size_t posicion)
     if(!lista)
         return ERROR;
         
-    if(posicion >= lista->cantidad)
+    if(posicion >= lista->cantidad || lista_vacia(lista))
         return lista_insertar(lista, elemento);
 
     nodo_t* nodo_nuevo = malloc(sizeof(nodo_t));
@@ -66,8 +66,9 @@ int lista_insertar_en_posicion(lista_t* lista, void* elemento , size_t posicion)
 }
 
 int lista_borrar(lista_t* lista){
-    
-    if(!lista || lista->cantidad < 1)
+    if(!lista)
+        return ERROR;
+    if(lista_vacia(lista))
         return ERROR;
 
     if(lista->cantidad > 1){
@@ -89,6 +90,78 @@ int lista_borrar(lista_t* lista){
     return SIN_ERROR;
 }
 
+
+/*
+*
+*/
+
+void destruir_nodos(nodo_t* nodo){
+    if(!nodo)
+        return;
+
+    if(nodo->siguiente)
+        destruir_nodos(nodo->siguiente);
+        
+    free(nodo);
+}
+
 void lista_destruir(lista_t* lista){
+    destruir_nodos(lista->nodo_inicio);
     free(lista);
+}
+
+int lista_borrar_de_posicion(lista_t* lista, size_t posicion){
+    if(!lista)
+        return ERROR;
+    if(lista_vacia(lista))
+        return ERROR;
+    
+    if((posicion >= (lista->cantidad - 1)) || (lista->cantidad == 1))
+        return lista_borrar(lista);
+
+    if(posicion == 0){
+        nodo_t* nodo_a_liberar = lista->nodo_inicio;
+        lista->nodo_inicio = nodo_a_liberar->siguiente;
+        free(nodo_a_liberar);
+    } else {
+        nodo_t* nodo_de_lista = lista->nodo_inicio;
+        for(int i = 0; i < posicion - 1; i++){              // no se si con iterador se podrá mejorar
+            nodo_de_lista = nodo_de_lista->siguiente;
+        }
+        nodo_t* nodo_a_liberar = nodo_de_lista->siguiente;
+        nodo_de_lista->siguiente = nodo_a_liberar->siguiente;
+        free(nodo_a_liberar);
+    }
+
+    lista->cantidad--;
+    
+    return SIN_ERROR;
+}
+
+void* lista_elemento_en_posicion(lista_t* lista, size_t posicion){
+    if(!lista)
+        return NULL;
+    if(lista_vacia(lista))
+        return NULL;
+    if(posicion >= lista->cantidad)
+        return NULL;
+    nodo_t* nodo_de_lista = lista->nodo_inicio;
+    for(int i = 0; i < posicion; i++){              // no se si con iterador se podrá mejorar
+        nodo_de_lista = nodo_de_lista->siguiente;
+    }
+    return nodo_de_lista->elemento;
+}
+
+void* lista_ultimo(lista_t* lista){
+    if(!lista)
+        return NULL;
+    if(lista_vacia(lista))
+        return NULL;
+    return lista->nodo_fin->elemento;
+}
+
+bool lista_vacia(lista_t* lista){
+    if(!lista)
+        return false;
+    return !(lista->cantidad);
 }
--- mis_pruebas.c
+++ mis_pruebas.c
@@ -2,7 +2,7 @@
 #include "pa2mm.h"
 #include <stdio.h>
 
-void probar_lista_crear(){
+void probar_lista_crear_y_destruir(){
     pa2m_nuevo_grupo("Pruebas crear lista:");
     lista_t* lista = lista_crear();
     pa2m_afirmar(lista != NULL, "Puntero a lista distinto de null.")
@@ -31,8 +31,6 @@ void probar_lista_insercion(){
     pa2m_afirmar(lista->cantidad == 3, "La lista tiene 3 elementos");
     pa2m_afirmar(lista->nodo_fin->elemento == &tercer_valor, "3er elemento correcto");
 
-    for(int i = 0; i < 3; i++)
-        lista_borrar(lista);
     lista_destruir(lista);
 }
 
@@ -50,20 +48,105 @@ void probar_lista_insercion_en_posicion(){ //PROBAR MEJOR CUANDO PUEDA ACCERDER 
     pa2m_afirmar(lista_insertar_en_posicion(lista, &sexto_valor, 2) == 0, "Insertar al medio.");
     pa2m_afirmar(lista->cantidad == 6, "Se actualiza la cantidad correctamente");
     
-    for(int i = 0; i < 6; i++)
-        lista_borrar(lista);
     lista_destruir(lista);
 }
 
 void probar_lista_borrar(){
-    //VAMO POR ACA
+    pa2m_nuevo_grupo("Pruebas borrar última posición");
+    lista_t* lista = lista_crear();
+    pa2m_afirmar(lista_borrar(NULL) == -1, "Devuelve error si la lista no tiene elementos");
+    int primer_valor = 1, segundo_valor = 2, tercer_valor = 3;
+    lista_insertar(lista, &primer_valor);
+    lista_insertar(lista, &segundo_valor);
+    lista_insertar(lista, &tercer_valor);
+    pa2m_afirmar(lista->cantidad == 3, "Hay 3 elementos");
+    pa2m_afirmar(lista_borrar(NULL) == -1, "Devuelve error si recibe NULL");
+    lista_borrar(lista);
+    pa2m_afirmar(lista->cantidad == 2, "Hay 2 elementos");
+    lista_borrar(lista);
+    pa2m_afirmar(lista->cantidad == 1, "Hay 1 elementos");
+    lista_borrar(lista);
+    pa2m_afirmar(lista->cantidad == 0, "La lista no tiene elementos");
+    lista_destruir(lista);
+}
+
+void probar_lista_borrar_de_posicion(){
+    pa2m_nuevo_grupo("Pruebas borrar en cualquier posición");
+    lista_t* lista = lista_crear();
+    pa2m_afirmar(lista_borrar_de_posicion(NULL, 0) == -1, "Devuelve error si la lista no tiene elementos");
+    int primer_valor = 1, segundo_valor = 2, tercer_valor = 3, cuarto_valor = 4, quinto_valor = 5;
+    lista_insertar(lista, &primer_valor);
+    lista_insertar(lista, &segundo_valor);
+    lista_insertar(lista, &tercer_valor);
+    lista_insertar(lista, &cuarto_valor);
+    lista_insertar(lista, &quinto_valor);
+    pa2m_afirmar(lista_borrar_de_posicion(NULL, 0) == -1, "Devuelve error si recibe NULL");
+    pa2m_afirmar(lista->cantidad == 5, "Hay 5 elementos");
+    pa2m_afirmar(lista_borrar_de_posicion(lista, 3) == 0, "Borro del medio");
+    pa2m_afirmar(lista->cantidad == 4, "Hay 4 elementos");
+    pa2m_afirmar(lista_borrar_de_posicion(lista, 0) == 0, "Borro del inicio");
+    pa2m_afirmar(lista->cantidad == 3, "Hay 3 elementos");
+    pa2m_afirmar(lista_borrar_de_posicion(lista, 0) == 0, "Borro del final");
+    pa2m_afirmar(lista->cantidad == 2, "Hay 2 elementos");
+    lista_destruir(lista);
+}
+
+void probar_lista_elemento_en_posicion(){
+    pa2m_nuevo_grupo("Pruebas elemento de cualquier posición");
+    lista_t* lista = lista_crear();
+    pa2m_afirmar(lista_elemento_en_posicion(NULL, 0) == NULL, "Devuelve NULL si la lista no tiene elementos");
+    int primer_valor = 1, segundo_valor = 2, tercer_valor = 3, cuarto_valor = 4, quinto_valor = 5;
+    lista_insertar(lista, &primer_valor);
+    lista_insertar(lista, &segundo_valor);
+    lista_insertar(lista, &tercer_valor);
+    lista_insertar(lista, &cuarto_valor);
+    lista_insertar(lista, &quinto_valor);
+    pa2m_afirmar(lista_elemento_en_posicion(NULL, 0) == NULL, "Devuelve NULL si recibe NULL como lista");
+    pa2m_afirmar(lista_elemento_en_posicion(lista, 66) == NULL, "Devuelve NULL si recibe una posicion inexistente de la lista");
+    pa2m_afirmar(lista_elemento_en_posicion(lista, 0) == &primer_valor, "Primer valor correcto");
+    pa2m_afirmar(lista_elemento_en_posicion(lista, 2) == &tercer_valor, "Ultimo valor correcto");
+    pa2m_afirmar(lista_elemento_en_posicion(lista, 4) == &quinto_valor, "Ultimo valor correcto");
+    lista_destruir(lista);
+}
+
+void probar_lista_ultimo(){
+    pa2m_nuevo_grupo("Pruebas ultimo elemento");
+    lista_t* lista = lista_crear();
+    pa2m_afirmar(lista_ultimo(lista) == NULL, "Devuelve NULL si la lista no tiene elementos");
+    int primer_valor = 1, segundo_valor = 2, tercer_valor = 3;
+    lista_insertar(lista, &primer_valor);
+    pa2m_afirmar(lista_ultimo(NULL) == NULL, "Devuelve NULL si recibe NULL como lista");
+    pa2m_afirmar(lista_ultimo(lista) == &primer_valor, "Valor correcto cuando hay 1");
+    lista_insertar(lista, &segundo_valor);
+    lista_insertar(lista, &tercer_valor);
+    pa2m_afirmar(lista_ultimo(lista) == &tercer_valor, "Valor correcto cuando hay 3");
+    lista_borrar(lista);
+    pa2m_afirmar(lista_ultimo(lista) == &segundo_valor, "Valor correcto Luego de borrar ultimo nodo");
+    lista_destruir(lista);
+}
+
+void probar_lista_vacia(){
+    pa2m_nuevo_grupo("Pruebas lista vacia");
+    lista_t* lista = lista_crear();
+    pa2m_afirmar(!lista_ultimo(NULL), "Devuelve false si recibe null como lista");
+    pa2m_afirmar(lista_vacia(lista), "Esta vacia luego de la creacion")
+    int primer_valor = 1;
+    lista_insertar(lista, &primer_valor);
+    pa2m_afirmar(!lista_vacia(lista), "No esta vacia luego de insertar elemento");
+    lista_borrar(lista);
+    pa2m_afirmar(lista_vacia(lista), "Esta vacia luego de borrar el elemento insertado")
+    lista_destruir(lista);
 }
 
 int main(){
-    probar_lista_crear();
+    probar_lista_crear_y_destruir();
     probar_lista_insercion();
     probar_lista_insercion_en_posicion();
     probar_lista_borrar();
+    probar_lista_borrar_de_posicion();
+    probar_lista_elemento_en_posicion();
+    probar_lista_ultimo();
+    probar_lista_vacia();
     pa2m_mostrar_reporte();
     return 0;
 }
